5a9e3dd4f075743a084e565064b644d2ced7c986
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-09-16 14:02:54 +0530

    Add files via upload

---
+++ Factorial1/Helper.c
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////
+//
+// Function name : Factorial
+// Input :         Integer
+// Output :        Integer
+// Description :   It is use to perform Factorial of number
+// Author :        Priyanka Sapakal
+// Date:           07/09/2021
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+  Algorithm
+  
+  START
+      Accept  number as iNo
+      Perform Factorial from 1 to iNo
+           iFact=iFact*iCnt;
+      Store the result in RET
+      Return the value of RET
+  STOP
+*/
+
+
+
+#include"Header.h"
+
+UINT Factorial(int iValue)
+{
+  UINT iFact=1;
+  register int iCnt=0;
+  if(iValue<0)
+  {
+    iValue=-iValue;
+  }
+
+  for(iCnt=1;iCnt<=iValue;iCnt++)
+  {
+     iFact=iFact*iCnt;
+   }
+ return iFact;
+}
