46d90739d66186fa40a28923282468c4fa5d9760
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-09-20 21:43:48 +0530

    commit

---
+++ Pattern4/Main.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Accept number from user and display below pattern.
+//                  
+//
+//////////////////////////////////////////////////////////////////////
+
+//Input:5
+//Output:2 4 6 8 10
+
+
+#include"Header.h"
+
+int main()
+{
+  int iValue=0;
+ 
+  printf("Enter the number\n");
+  scanf("%d",&iValue);
+
+  Display(iValue);
+ 
+
+ return 0;
+}
