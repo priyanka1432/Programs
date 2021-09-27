6f00230f07129f164839cfa2702e99fde9b0d9a4
priyanka <priyankasapakal173@gmail.com>
2021-09-27 18:00:26 +0530

    initial commit

--- Array/Helper.c
+++ Array/Helper.c
@@ -49,31 +49,7 @@ int AddArray(int *Arr,int iSize)
 }
 
 
-//////////////////////////////////////////////////////////////////////////
-//
-// Function name : AddArray
-// Input :         Integer
-// Description :   It is used to perform addition of array.
-// Author :        Priyanka Sapakal
-// Date:           22/09/2021
-//
-//////////////////////////////////////////////////////////////////////////
 
-
-#include"Header.h"
-
-int AdditionArray(int *Arr,int iSize)
-{
-     
-     int iSum=0;
-     for(int iCnt=0;iCnt<iSize;iCnt++)
-     {
-       iSum=iSum+(*Arr);
-       Arr++;
-      }
-     
-   return iSum;
-}
 
 
 
