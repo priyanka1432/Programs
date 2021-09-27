0c520d8851d1478573a592880b418b900f7fa31f
priyanka <priyankasapakal173@gmail.com>
2021-09-27 13:26:00 +0530

    updated code

--- pattern/Helper.c
+++ pattern/Helper.c
@@ -86,3 +86,29 @@ void Pattern3(int iNo)
 
 
 
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Function name : Pattern4
+// Input :         Integer
+// Description :   It is use to Display Pattern.
+// Author :        Priyanka Sapakal
+// Date:           20/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+#include"Header.h"
+
+void Pattern4(int iNo)
+{
+    register int iCnt =0;
+  
+
+     for(iCnt=1;iCnt<=iNo;iCnt++)
+     {
+            printf("%d\t",iCnt*2);
+      }   
+}
+
+
---
+++ pattern/Pattern4.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Accept number from user and display below pattern.
+//                  
+//
+//////////////////////////////////////////////////////////////////////
+
+//Input:5
+//Output:2 4 6 8 10
+
+
+#include"Header.h"
+
+int main()
+{
+  int iValue=0;
+ 
+  printf("Enter the number\n");
+  scanf("%d",&iValue);
+
+  Pattern4(iValue);
+ 
+
+ return 0;
+}
